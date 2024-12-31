8e847b7be55b091819c4fcfc65ebaba1f9ba19c7
shayan taki <takishayan@gmail.com>
2024-12-31 19:45:36 +0330

    add  frontend

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    model = User
+    list_display = ('username', 'is_active', 'is_staff', 'date_joined')
+    readonly_fields = ('date_joined', 'last_login') # افزودن date_joined به readonly_fields
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+    search_fields = ('username',)
+    ordering = ('username',)
+
+admin.site.register(User, CustomUserAdmin)
