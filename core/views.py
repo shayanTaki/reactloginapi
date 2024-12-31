f043d5c2b43600b1264edc253a37a7ea9fca3fe6
shayan taki <takishayan@gmail.com>
2024-12-31 19:15:48 +0330

    add urls and views

--- core/views.py
+++ core/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
+from django.contrib.auth import login
+from .serializers import UserSerializer, LoginSerializer
 
-# Create your views here.
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def register_view(request):
+    serializer = UserSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def login_view(request):
+    serializer = LoginSerializer(data=request.data)
+    if serializer.is_valid():
+        user = serializer.validated_data['user']
+        login(request, user)
+        return Response({'detail': 'با موفقیت وارد شدید.'}, status=status.HTTP_200_OK)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
